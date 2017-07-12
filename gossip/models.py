b5a33f0e9a6890362125b612e0efc5df7dbedf94
scotty-b <businge.scott@andela.com>
2017-07-12 21:27:28 +0300

    modify models file

--- gossip/models.py
+++ gossip/models.py
@@ -8,7 +8,7 @@ class Link(models.Model):
     submitted_on = models.DateTimeField(auto_now_add=True)
     rank_score = models.FloatField(default=0.0)
     url = models.URLField("URL", max_length=250, blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(max_length=300, blank=True)
 
     def __unicode__(self):
         return self.title
