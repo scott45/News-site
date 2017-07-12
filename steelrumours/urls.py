2d576f87f50b0b37edb9466853ac037839d4ebdc
scotty-b <businge.scott@andela.com>
2017-07-12 21:41:04 +0300

    add home urls

--- steelrumours/urls.py
+++ steelrumours/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
+from gossip.views import LinkListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', LinkListView.as_view(), name='home'),
 ]
