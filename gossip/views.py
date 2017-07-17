f149523e4dd7af3ce5ca04a9f7ef96814adee48d
scotty-b <businge.scott@andela.com>
2017-07-17 15:42:47 +0300

    add views for CRUD functionalities

--- gossip/views.py
+++ gossip/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import Link
-from django.contrib.auth import authenticate
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, logout
 from .forms import UserForm
 from django.shortcuts import render
+from gossip.forms import LinkForm
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 def register(request):
@@ -17,7 +19,7 @@ def register(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                return render(request, 'index.html')
+                return render(request, 'gossip/link_list.html')
     context = {
         "form": form,
     }
@@ -31,7 +33,7 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                return render(request, 'index.html')
+                return render(request, 'gossip/link_list.html')
             else:
                 return render(request, 'registration/login.html', {'error_message': 'Your account has been disabled'})
         else:
@@ -50,7 +52,35 @@ def logout_user(request):
 
 class LinkListView(ListView):
     model = Link
-    paginate_by = 10
+    template_name = 'gossip/link_list.html'
+    paginate_by = 5
 
     def get_query_set(self):
         return Link.with_votes.all(self)
+
+
+class LinkCreateView(CreateView):
+    model = Link
+    form_class = LinkForm
+
+    def form_valid(self, form):
+        f = form.save(commit=False)
+        f.rank_score = 0.0
+        f.submitter = self.request.user
+        f.save()
+
+        return super(CreateView, self).form_valid(form)
+
+
+class LinkDetailView(DetailView):
+    model = Link
+
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    form_class = LinkForm
+
+
+class LinkDeleteView(DeleteView):
+    model = Link
+    success_url = reverse_lazy("home")
