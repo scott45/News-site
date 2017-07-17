2f077881977bdab5b2b458e5aeced594aaecad83
scotty-b <businge.scott@andela.com>
2017-07-17 15:41:54 +0300

    Include CRUD views

--- gossip/forms.py
+++ gossip/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from gossip.models import Link
 from django.contrib.auth.models import User
 
 
@@ -8,3 +9,9 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+
+class LinkForm(forms.ModelForm):
+        class Meta:
+            model = Link
+            exclude = ("submitter", "rank_score")
