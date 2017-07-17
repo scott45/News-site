dc6c4a71c7bd802f7635c8b67d8b207b83354aa3
scotty-b <businge.scott@andela.com>
2017-07-17 11:37:57 +0300

    add views for register, login and logout

--- gossip/views.py
+++ gossip/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView
 from .models import Link
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
+from .forms import UserForm
+from django.shortcuts import render
 
 
 class LinkListView(ListView):
@@ -8,3 +12,45 @@ class LinkListView(ListView):
 
     def get_query_set(self):
         return Link.with_votes.all(self)
+
+
+def register(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                return render(request, 'index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'registration/register.html', context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                return render(request, 'index.html')
+            else:
+                return render(request, 'registration/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'registration/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'registration/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'registration/login.html', context)
